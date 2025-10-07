db85c2ef2dc0f944c3dcf9ab035f4ec80ae96f8f
webdmitriev <webdmitriev@gmail.com>
2025-10-07 10:53:17 +0300

    Added popup forms

--- webdmitriev/popup.php
+++ webdmitriev/popup.php
@@ -17,3 +17,13 @@
     <?= do_shortcode('[contact-form-7 id="adb2625" title="Форма участия"]'); ?>
   </div>
 </div>
+
+
+<!-- btn-web-popup-default / web-popup-default -->
+<div class="web-popup web-popup-default">
+  <div class="popup-bg"></div>
+  <div class="close-web-popup"></div>
+  <div class="popup-content scroll-line-none">
+    <?= do_shortcode('[contact-form-7 id="0bda2c0" title="Default"]'); ?>
+  </div>
+</div>
