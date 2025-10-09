1a442b5d3e619f54b970e62727318ebd6c9b6539
webdmitriev <webdmitriev@gmail.com>
2025-10-09 17:31:18 +0300

    added ico

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?= $url; ?>/favicon.ico" type="image/x-icon">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
